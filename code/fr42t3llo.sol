ddbad1f452c2a40d36bea2f724e5f3d895746326
FRATELLO <saife.addine123@gmail.com>
2025-04-23 23:31:26 +0100

    update by FRATELLO

--- code/fr42t3llo.sol
+++ code/fr42t3llo.sol
@@ -2,7 +2,6 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.22;
 
- 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Fr42t3llo is ERC20 {
